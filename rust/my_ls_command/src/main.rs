8008158e1852e5a8d9c47a8cc8a39b12541e9f83
troyw1987 <troygregorywilson1@gmail.com>
2024-08-27 10:29:15 -0700

    made a shitty ls command :)

--- rust/my_ls_command/src/main.rs
+++ rust/my_ls_command/src/main.rs
@@ -1,11 +1,19 @@
-use std::env;
-use std::path::{Display, PathBuf};
+use std::path::PathBuf;
+use std::{env, fs};
 
 fn main() {
     let totest_path: Result<PathBuf, std::io::Error> = env::current_dir();
-    let raw_path: PathBuf = totest_path.expect("yer");
 
-    let out: Display = raw_path.display();
+    let raw_path: PathBuf = totest_path.expect("yer");
+    let mut out: String = String::new();
+
+    let paths = fs::read_dir(raw_path);
+
+    for path in paths.unwrap() {
+        let raw_entry = path.unwrap().file_name();
+        let entry = raw_entry.to_str().unwrap();
+        out = out + entry + "\n";
+    }
 
     println!("{out}\n");
 }
