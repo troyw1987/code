8c18b4ad0d47bac34c2596355d0ca8a3d4ca34cd
troyw1987 <troygregorywilson1@gmail.com>
2024-08-29 07:48:24 -0700

    finalized the output for the solver.

--- lua/synthetic_division_solver/main.lua
+++ lua/synthetic_division_solver/main.lua
@@ -28,23 +28,45 @@ local function synthetic_division(number_table, divisor)
 	return answer_table
 end
 
+local function format_answer(answer_table)
+	local output = ""
+	for i = #answer_table, 0, -1 do
+		local current = ""
+		if i >= 2 then
+			current = (answer_table[i] .. "x" .. "^" .. i)
+		else
+			current = (answer_table[i] .. "^" .. i)
+		end
+
+		if answer_table[i] >= 0 then
+			current = "+" .. current
+		end
+
+		output = output .. current .. " "
+	end
+	return output
+end
+
 local problem_table = {
-	[0] = 8,
-	[1] = -10,
-	[2] = 1,
-	[3] = 1,
+	[0] = -4,
+	[1] = -2,
+	[2] = 4,
+	[3] = 2,
 }
 
 local function main()
-	local answer_table = synthetic_division(problem_table, 1)
+	local answer_table = synthetic_division(problem_table, -2)
 
 	if answer_table[0] == 0 then
-		print("HALELUJA")
+		print("    HALELUJA")
 	end
 
-	for i = 3, 0, -1 do
-		print(i, answer_table[i])
+	for i, v in pairs(answer_table) do
+		print(i, "^", v)
 	end
+
+	local answer = format_answer(answer_table)
+	print(answer)
 end
 
 main()
